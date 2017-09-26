4e36905df2e8c6ad00fc48e85efd6473cec31190
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-09-26 14:52:59 -0400

    Get doctor by id route added

--- server/routes.js
+++ server/routes.js
@@ -6,7 +6,7 @@ import doctorCtrl from './controllers/doctorCtrl';
 // Doctor routes
 routes.post('/doctors/new', doctorCtrl.newDoctor);
 routes.get('/doctors', doctorCtrl.getAllDoctors);
-// routes.get('/doctors/:id', doctorCtrl.getDoctorById);
+routes.get('/doctors/:id', doctorCtrl.getDoctorById);
 // routes.put('/doctors/update/:id', doctorCtrl.put);
 // routes.delete('/doctors/delete/:id', doctorCtrl.delete);
 
