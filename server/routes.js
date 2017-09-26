99c270dcaebc6d71536c30fe7f63773a1cba4d74
Leonardo Andres <leonardoandres.1298@gmail.com>
2017-09-26 12:32:22 -0400

    Routes and doctorCtrl modified

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,8 @@ const routes = express();
 import doctorCtrl from './controllers/doctorCtrl';
 
 // Doctor routes
-routes.post('/doctors/add', doctorCtrl.post);
+routes.post('/doctors/new', doctorCtrl.post);
 routes.get('/doctors', doctorCtrl.getAll);
+routes.get('/doctors/:speciality?', doctorCtrl.getBySpeciality);
 
 export default routes;
